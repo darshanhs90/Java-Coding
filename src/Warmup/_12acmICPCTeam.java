4c6fe570af77f73f926062caf13f94b9b77fea75
darshanhs90 <hsdars@gmail.com>
2015-06-09 18:10:53 +0530

    updated

--- src/Warmup/_12acmICPCTeam.java
+++ src/Warmup/_12acmICPCTeam.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 
 public class _12acmICPCTeam {
 	public static void main(String[] args) throws  IOException {
-		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		/*BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
 		String firstLine=scanner.readLine();
 		String flArray[]=firstLine.split(" ");
 		int N=Integer.parseInt(flArray[0]);
 		int M=Integer.parseInt(flArray[1]);
-		ArrayList<Integer> peopleArray=new ArrayList<Integer>();
-		ArrayList<Integer> outputArray=new ArrayList<Integer>();
+		ArrayList<String> peopleArray=new ArrayList<String>();
+		ArrayList<String> outputArray=new ArrayList<String>();
 		for (int noOfTestCases = 0; noOfTestCases < N; noOfTestCases++) {
-			int a=0;
-			int b=0;
-
-		}
+			peopleArray.add(scanner.readLine());
+		}*/
+		System.out.println(Integer.decode("1101"));
+		System.out.println(Integer.toString((Integer.decode("1101")|Integer.decode("1011"))));
 	}
 }
