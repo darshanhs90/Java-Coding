1c8333c9ff4205af30de0953a04946d23083ed4a
darshanhs90 <hsdars@gmail.com>
2015-06-09 10:01:53 +0530

    12 and 13 temp commit

---
+++ src/Warmup/_12acmICPCTeam.java
@@ -0,0 +1,23 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class _12acmICPCTeam {
+	public static void main(String[] args) throws  IOException {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		String firstLine=scanner.readLine();
+		String flArray[]=firstLine.split(" ");
+		int N=Integer.parseInt(flArray[0]);
+		int M=Integer.parseInt(flArray[1]);
+		ArrayList<Integer> peopleArray=new ArrayList<Integer>();
+		ArrayList<Integer> outputArray=new ArrayList<Integer>();
+		for (int noOfTestCases = 0; noOfTestCases < N; noOfTestCases++) {
+			int a=0;
+			int b=0;
+
+		}
+	}
+}
---
+++ src/Warmup/_13taumAndBday.java
@@ -0,0 +1,23 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _13taumAndBday {
+	public static void main(String[] args) throws IOException {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		int T=Integer.parseInt(scanner.readLine());
+		for (int noOfTestCases = 0; noOfTestCases < T; noOfTestCases++) {
+			String flArray[]=scanner.readLine().split(" ");
+			int B=Integer.parseInt(flArray[0]);
+			int W=Integer.parseInt(flArray[1]);
+			String slArray[]=scanner.readLine().split(" ");
+			int X=Integer.parseInt(flArray[0]);
+			int Y=Integer.parseInt(flArray[1]);
+			int Z=Integer.parseInt(flArray[2]);
+			
+			
+		}
+	}
+}
