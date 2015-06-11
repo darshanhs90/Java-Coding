6343f69bfa85a5b619903f5870fc90bd59894503
darshanhs90 <hsdars@gmail.com>
2015-06-11 09:03:19 +0530

    14 started

---
+++ src/Warmup/_14sherlockAndSquares.java
@@ -0,0 +1,19 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _14sherlockAndSquares {
+	public static void main(String[] args) throws IOException {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		long T=Long.parseLong(scanner.readLine());
+		for (long noOfTestCases = 0; noOfTestCases < T; noOfTestCases++) {
+			String sline[]=scanner.readLine().split(" ");
+			long A=Long.parseLong(sline[0]);
+			long B=Long.parseLong(sline[1]);
+			
+			
+		}
+	}
+}
