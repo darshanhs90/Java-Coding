7c1bb9cd71a7af249817781e220d8f361ef614cc
darshanhs90 <hsdars@gmail.com>
2015-06-11 09:35:42 +0530

    15 Started

---
+++ src/Warmup/_15sherlockAndGCD.java
@@ -0,0 +1,16 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15sherlockAndGCD {
+	public static void main(String[] args) throws IOException {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		long T=Long.parseLong(scanner.readLine());
+		for (long noOfTestCases = 0; noOfTestCases < T; noOfTestCases++) {
+			
+
+		}
+	}
+}
