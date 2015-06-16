7f9cf32f7232753db19af3d84a7a2785e9627230
darshanhs90 <hsdars@gmail.com>
2015-06-16 08:39:26 +0530

    18 added

---
+++ src/Warmup/_18maxMin.java
@@ -0,0 +1,16 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _18maxMin {
+	public static void main(String[] args) throws IOException {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		long N=Long.parseLong(scanner.readLine());
+		long K=Long.parseLong(scanner.readLine());
+		for (long noOfLines = 0; noOfLines < N; noOfLines++) {
+			
+		}
+	}
+}
