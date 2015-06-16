ed7534356c82c369ba627f8176e03087ed0425ad
darshanhs90 <hsdars@gmail.com>
2015-06-16 07:00:51 +0530

    16 Sherlock and the Beast Accepted

--- src/Warmup/_16sherlockAndTheBeast.java
+++ src/Warmup/_16sherlockAndTheBeast.java
@@ -3,14 +3,43 @@ package Warmup;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class _16sherlockAndTheBeast {
 	public static void main(String[] args) throws IOException {
 		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
 		long T=Long.parseLong(scanner.readLine());
 		for (long noOfTestCases = 0; noOfTestCases < T; noOfTestCases++) {
-			
+			long N=Long.parseLong(scanner.readLine());
+			long noOfFives=N;
+			long noOfThrees=0;
+			boolean num=false;
+			while(true)
+			{
+				if(noOfFives%3==0 && noOfThrees%5==0)
+				{	
+					num=true;
+					break;
+				}
+				else
+				{
+					noOfFives--;
+					noOfThrees++;
+				}
+				if(noOfFives<0)
+					break;
+			}
+			if(num==true){
+				StringBuilder sb=new StringBuilder();
+				for (int i = 0; i < noOfFives; i++) {
+					sb.append('5');
+				}
+				for (int i = 0; i < noOfThrees; i++) {
+					sb.append('3');
+				}
+				System.out.println(sb.toString());
+			}
+			else
+				System.out.println("-1");
 		}
 	}
 }
