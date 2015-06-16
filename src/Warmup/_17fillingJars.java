20984a5fa471e4648b714013c560f5cfed7f4cab
darshanhs90 <hsdars@gmail.com>
2015-06-16 08:29:02 +0530

    17 updated

--- src/Warmup/_17fillingJars.java
+++ src/Warmup/_17fillingJars.java
@@ -3,14 +3,21 @@ package Warmup;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class _17fillingJars {
 	public static void main(String[] args) throws IOException {
 		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
-		long T=Long.parseLong(scanner.readLine());
-		for (long noOfTestCases = 0; noOfTestCases < T; noOfTestCases++) {
-			
+		String fLine[]=(scanner.readLine().split(" "));
+		long N=Long.parseLong(fLine[0]);
+		long M=Long.parseLong(fLine[1]);
+		long total=0l;
+		for (long noOfOperations = 0; noOfOperations < M; noOfOperations++) {
+			String operationsLine[]=scanner.readLine().split(" ");
+			long a=Long.parseLong(operationsLine[0]);
+			long b=Long.parseLong(operationsLine[1]);
+			long noOfCandies=Long.parseLong(operationsLine[2]);
+			total+=(b-a+1)*noOfCandies;
 		}
+		System.out.println((int)Math.floor(total/N));
 	}
 }
