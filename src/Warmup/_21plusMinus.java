c0c42f27ad4ca994ca4be75f9729edb14a333352
Haridarshan HS <hsdars@gmail.com>
2015-07-18 03:03:23 -0500

    21.plusMinus added

---
+++ src/Warmup/_21plusMinus.java
@@ -0,0 +1,12 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _21plusMinus {
+	public static void main(String[] args) throws Exception {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(scanner.readLine());
+
+	}
+}
