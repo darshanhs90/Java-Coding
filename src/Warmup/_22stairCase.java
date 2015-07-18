f8ad8a71bfc22608fb7ca2de0f734fb570b32807
Haridarshan HS <hsdars@gmail.com>
2015-07-18 13:30:38 -0500

    22.stair case completed

---
+++ src/Warmup/_22stairCase.java
@@ -0,0 +1,26 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _22stairCase {
+	public static void main(String[] args) throws Exception {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(scanner.readLine());
+		String hashString="";
+		String emptyString="";
+		
+		for (int i = 0; i < N; i++) {
+			hashString+="#";
+			emptyString+=" ";
+		}
+		for (int i = 0; i < N; i++) {
+			String formatHash="%."+i+"s";
+			String formatSpace="%."+(N-i-1)+"s";
+			System.out.format(formatSpace,emptyString);
+			System.out.format(formatHash,hashString);
+			System.out.print("#");
+			System.out.println();
+		}
+	}
+}
