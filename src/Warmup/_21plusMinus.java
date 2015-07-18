095a95fc18d49bb8d03833d4ec95c85ed94b5726
Haridarshan HS <hsdars@gmail.com>
2015-07-18 12:17:51 -0500

    21.plus minus completed

--- src/Warmup/_21plusMinus.java
+++ src/Warmup/_21plusMinus.java
@@ -7,6 +7,23 @@ public class _21plusMinus {
 	public static void main(String[] args) throws Exception {
 		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(scanner.readLine());
+		String  line=scanner.readLine();
+		String lineArray[]=line.split(" ");
+		int posCount=0,negCount=0,zeroCount=0;
+		for(int i=0;i<lineArray.length;i++){
+			if(Integer.parseInt(lineArray[i])>0)
+				posCount++;
+			else if(Integer.parseInt(lineArray[i])<0)
+				negCount++;
+			else
+				zeroCount++;
+		}
 
+		System.out.printf("%.3f",((double)posCount/N));
+		System.out.println();
+		System.out.printf("%.3f",((double)negCount/N));
+		System.out.println();
+		System.out.printf("%.3f",((double)zeroCount/N));
+		
 	}
 }
