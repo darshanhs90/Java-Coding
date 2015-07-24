117be6067db4e7e88e913cfe0d2e04bb4edab2b9
Haridarshan HS <hsdars@gmail.com>
2015-07-23 21:49:48 -0500

    32 matrixRotation started

---
+++ src/Warmup/_32matrixRotation.java
@@ -0,0 +1,17 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _32matrixRotation {
+	public static void main(String[] args) throws Exception {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		String input=scanner.readLine();
+		String inputArray[]=input.split(" ");
+		int M=Integer.parseInt(inputArray[0]);
+		int N=Integer.parseInt(inputArray[1]);
+		int R=Integer.parseInt(inputArray[2]);
+		
+		
+	}
+}
