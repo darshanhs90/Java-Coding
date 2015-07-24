0856f7682af2d9ab10b175164d5debc68711cfed
Haridarshan HS <hsdars@gmail.com>
2015-07-24 01:25:33 -0500

    32 matrix rotation added

--- src/Warmup/_32matrixRotation.java
+++ src/Warmup/_32matrixRotation.java
@@ -11,7 +11,22 @@ public class _32matrixRotation {
 		int M=Integer.parseInt(inputArray[0]);
 		int N=Integer.parseInt(inputArray[1]);
 		int R=Integer.parseInt(inputArray[2]);
+		int mainArray[][]=new int[M][N];
+		int finalArray[][]=new int[M][N];
+		for (int i = 0; i < M; i++) {
+			String rowValue=scanner.readLine();
+			String rowArray[]=rowValue.split(" ");
+			for (int j = 0; j < N; j++) {
+				mainArray[i][j]=Integer.parseInt(rowArray[j]);
+			}
+		}
 		
-		
+		for (int i = 0; i < R/2; i++) {//number of outer regions
+			
+			
+			
+			
+			
+		}
 	}
 }
