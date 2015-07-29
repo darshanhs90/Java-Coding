de1e4bfe637c935d73868c8b81858c315a0103be
Haridarshan HS <hsdars@gmail.com>
2015-07-29 08:58:07 -0500

    40 bigger is greater started

---
+++ src/Warmup/_40biggerIsGreater.java
@@ -0,0 +1,17 @@
+package Warmup;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _40biggerIsGreater {
+	public static void main(String[] args) throws Exception {
+		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
+		int noOfTestCases=Integer.parseInt(scanner.readLine());
+		for (int i = 0; i < noOfTestCases; i++) {
+			String firstLine=scanner.readLine();
+
+
+
+		}
+	}
+}
