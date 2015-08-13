b5a3c989338dc92e10e4e976cf57c2fe01a9506a
Haridarshan HS <hsdars@gmail.com>
2015-08-12 19:12:17 -0500

    insertion sort method code modified

--- src/Warmup/_47insertionSortPart1.java
+++ src/Warmup/_47insertionSortPart1.java
@@ -2,20 +2,26 @@ package Warmup;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 //https://www.hackerrank.com/challenges/insertionsort1
+//5
+//2 4 6 8 3
 public class _47insertionSortPart1 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader scanner=new BufferedReader(new InputStreamReader(System.in));
 		int s=Integer.parseInt(scanner.readLine());
 		String inputLine=scanner.readLine();
 		String inputArray[]=inputLine.split(" ");
-		String modifiedArray[]=inputLine.split(" ");
-
+		insertionSort1(inputArray);
+		
+	}
+	public static String[] insertionSort1(String[] inputArray){
 		int rightPositionElement=Integer.parseInt(inputArray[inputArray.length-1]);
 		int positionCheck=inputArray.length-2;
 		int previousElement=0,presentElement=Integer.parseInt(inputArray[positionCheck]);
 		int index=-1;
+		String[] modifiedArray=Arrays.copyOf(inputArray,inputArray.length);
 		for (int i = modifiedArray.length-2; i >=0; i--) {
 			presentElement=Integer.parseInt(inputArray[i]);
 			if(previousElement!=presentElement){
@@ -43,6 +49,6 @@ public class _47insertionSortPart1 {
 		for (int j = 0; j < modifiedArray.length; j++) {
 			System.out.print(modifiedArray[j]+" ");
 		}
-		
+		return modifiedArray;
 	}
 }
