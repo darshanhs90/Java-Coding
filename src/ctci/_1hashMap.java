db47d2bdc466e2289145365e3655b22d8a4ace24
Haridarshan HS <hsdars@gmail.com>
2015-08-17 21:24:35 -0500

    hash map structure added

--- src/ctci/_1hashMap.java
+++ src/ctci/_1hashMap.java
@@ -1,6 +1,19 @@
 package ctci;
 /*Implementation of Hash Map Data Structure*/
 public class _1hashMap {
+
+	public void hashFunctionEncode(){
+
+	}
+	public void put(){
+
+	}
+	public void get(){
+
+	}
+	public void hashFunctionDecode(){
+
+	}
 	public static void main(String[] args) {
 
 	}
