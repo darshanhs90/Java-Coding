8201d2d44d0cd79f08f2fefa8a49b1d241fc81c4
Haridarshan HS <hsdars@gmail.com>
2015-08-19 16:59:37 -0500

    2.linkedlist implementation

--- src/ctci/_2linkedList.java
+++ src/ctci/_2linkedList.java
@@ -1,17 +1,42 @@
 package ctci;
 
-
 /*Implementation of Singly Linked List Data Structure*/
 public class _2linkedList {
 	private Node presentNode;
 	private Node headNode;
+	private int noOfElements=0;
 	public _2linkedList() {
 	}
 	public int get(int position){
 		return 0;
 	}
-	public void add(int value,int position){
-		
+	public boolean add(int value,int position){
+		//check for posn number using noOFelements
+		if(position>noOfElements)
+			return false;
+		if(position==0){
+			add(value);
+			return true;
+		}
+		//go from header till that position
+		Node pointerNode=new Node();
+		pointerNode=headNode;
+		int elementCount=0;
+		while(position!=elementCount){
+			//conditions,its at last
+			pointerNode=pointerNode.next;
+			elementCount++;
+			//not at last
+		}
+		Node newNode=new Node();
+		newNode.data=value;
+		newNode.next=pointerNode.next;
+		pointerNode.next=newNode;
+		noOfElements++;
+		return true;
+	}
+	public int size(){
+		return noOfElements;
 	}
 	public void add(int value){
 		if(presentNode==null){
@@ -27,6 +52,17 @@ public class _2linkedList {
 			newNode.data=value;
 			presentNode=newNode;
 		}
+		noOfElements++;
+	}
+	public boolean delete(){
+		if(headNode==null)
+			return false;
+		headNode=headNode.next;
+		return true;
+	}
+	public boolean delete(int position){
+		//to be implemented
+		return false;
 	}
 }
 class Node{
