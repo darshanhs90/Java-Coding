2c404840e8aafcd72e5be13652a628ff558b229b
Haridarshan HS <hsdars@gmail.com>
2015-08-21 01:46:08 -0500

    4.string buffer completed

--- src/ctci/_4stringBuffer.java
+++ src/ctci/_4stringBuffer.java
@@ -1,7 +1,52 @@
 package ctci;
 
+import java.util.Stack;
+
 
 /*Implementation of String Buffer Data Structure*/
 public class _4stringBuffer {
-	
+	String mainString;
+	public _4stringBuffer() {
+		// TODO Auto-generated constructor stub
+		mainString=new String();
+	}
+	public void append(String string){
+		mainString=new String(mainString+string);
+	}
+	public void append(char c){
+		mainString=new String(mainString+String.valueOf(c));
+	}
+	public void append(int i){
+		mainString=new String(mainString+i);
+	}
+	public int length(){
+		return mainString!=null?mainString.length():0;
+	}
+	public void delete(int startIndex,int endIndex){
+		mainString=new String(mainString.substring(0,startIndex)+mainString.substring(endIndex));
+	}
+	public void deleteCharAt(int i){
+		mainString=new String(mainString.substring(0,i)+mainString.substring(i+1));
+	}
+	public void setLength(int newLength){
+		if(newLength<mainString.length())
+			mainString=new String(mainString.substring(0,newLength));
+		else
+			mainString=new String(mainString+"\u0000");
+	}
+	public void reverse(){
+		Stack<Character> reverser=new Stack<Character>();
+		for (int i = 0; i < mainString.length(); i++) {
+			reverser.push(mainString.charAt(i));
+		}
+		int length=mainString.length();
+		mainString=new String();
+		for (int i = 0; i < length; i++) {
+			mainString+=reverser.pop();
+		}
+	}
+	public String toString(){
+		return mainString;
+	}
+
 }
--- src/ctci/_4stringBufferTest.java
+++ src/ctci/_4stringBufferTest.java
@@ -3,5 +3,25 @@ package ctci;
 
 /*Implementation of StringBuffer Data Structure Tester*/
 public class _4stringBufferTest {
-	
+	public static void main(String[] args) {
+		_4stringBuffer stringBuffer=new _4stringBuffer();
+		stringBuffer.append('c');
+		System.out.println(stringBuffer.toString());
+		stringBuffer.append("2ndstring");
+		System.out.println(stringBuffer.toString());
+		stringBuffer.append(1);
+		System.out.println(stringBuffer.toString());
+		System.out.println(stringBuffer.length());
+		stringBuffer.deleteCharAt(3);
+		System.out.println(stringBuffer.toString());
+		stringBuffer.delete(0,4);
+		System.out.println(stringBuffer.toString());
+		stringBuffer.setLength(4);
+		System.out.println(stringBuffer.toString());
+		stringBuffer.setLength(14);
+		System.out.println(stringBuffer.toString());
+		stringBuffer.reverse();
+		System.out.println(stringBuffer.toString());
+		
+	}
 }
