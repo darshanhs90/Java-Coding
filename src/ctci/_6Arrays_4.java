5215aa5eb6c525d0634325a1a3742acc5da12521
darshanhs90 <hsdars@gmail.com>
2015-08-24 00:50:42 +0530

    Arrays and String 1.5 completed

---
+++ src/ctci/_6Arrays_4.java
@@ -0,0 +1,39 @@
+package ctci;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+/*Implementation of CTCI 1.4*/
+public class _6Arrays_4 {
+	public static void main(String args[]){
+		Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		String inputString=scanner.nextLine();
+		System.out.println(compressedString(inputString));
+	}
+	public static String compressedString(String inputString){
+		String outputString="";
+		char previousChar=' ';
+		int previousCharCount=0;
+		for (int i = 0; i < inputString.length(); i++) {
+			char currentChar=inputString.charAt(i);
+			if(currentChar!=previousChar)
+			{
+				if(previousChar!=' ')
+					outputString+=previousCharCount;
+				previousChar=currentChar;
+				previousCharCount=1;
+				outputString+=currentChar;
+			}
+			else{
+				previousCharCount++;
+			}
+		}
+		outputString+=previousCharCount;
+		if(outputString.length()>inputString.length())
+			return inputString;
+		return outputString;
+	}
+
+
+}
