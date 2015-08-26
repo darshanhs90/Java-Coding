2d1446f53a26d37058b54447e45a6791220a5933
darshanhs90 <hsdars@gmail.com>
2015-08-27 02:58:55 +0530

    09.Binary Tree Implementation completed

---
+++ src/ctci/_09BinaryTree.java
@@ -0,0 +1,113 @@
+package ctci;
+
+
+/*Implementation of CTCI */
+public class _09BinaryTree{
+	private int noOfElements=0;
+	private Node rootNode;
+	private Node searchedNode;
+	public int size(){
+		return noOfElements;
+	}
+	public void insert(int value,String position,int parent){
+		if(rootNode==null){
+			rootNode=new Node(value, null,null);
+			noOfElements++;
+			return;
+		}
+		searchedNode=null;
+		getNode(rootNode,parent);
+		if(searchedNode!=null){
+			if(position.toLowerCase().contentEquals("left")){
+				if(searchedNode.left==null){
+					searchedNode.left=new Node(value,null,null);
+					noOfElements++;
+				}else{
+					System.out.println("Position already occupied");
+				}
+			}
+			else{
+				if(searchedNode.right==null){
+					searchedNode.right=new Node(value,null,null);
+					noOfElements++;
+				}else{
+					System.out.println("Position already occupied");
+				}
+			}
+		}
+	}
+
+	private void getNode(Node node,int parent) {
+		if(node.data==parent){
+			searchedNode=node;
+			return;
+		}
+		else{
+			if(node.left!=null)
+				getNode(node.left, parent);
+			if(node.right!=null)
+				getNode(node.right, parent);
+
+		}
+
+	}
+
+	public boolean search(int value){
+		searchedNode=null;
+		getNode(rootNode, value);
+		if(searchedNode!=null)
+			return true;
+		else
+			return false;
+	}
+	public void delete(int value){
+		searchedNode=null;
+		getNode(rootNode, value);
+		if(searchedNode!=null)
+		{
+			if(searchedNode.left!=null)
+			{
+				searchedNode.data=searchedNode.left.data;	
+				searchedNode=searchedNode.left;	
+				noOfElements--;
+			}
+			else if(searchedNode.right!=null)
+			{
+				searchedNode.data=searchedNode.right.data;
+				searchedNode=searchedNode.right;
+				noOfElements--;
+			}
+		}
+	}
+
+
+}
+class Node{
+	int data;
+	Node left=null;
+	Node right=null;
+	public Node(int data,Node left,Node right) {
+		this.data=data;
+		this.left=left;
+		this.right=right;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public Node getLeft() {
+		return left;
+	}
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	public Node getRight() {
+		return right;
+	}
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+}
---
+++ src/ctci/_09BinaryTreeTest.java
@@ -0,0 +1,19 @@
+package ctci;
+
+
+/*Implementation of CTCI */
+public class _09BinaryTreeTest{
+	public static void main(String[] args) {
+		_09BinaryTree binaryTree=new _09BinaryTree();
+		binaryTree.insert(10,"",0);
+		binaryTree.insert(11,"LEFT",10);
+		binaryTree.insert(12,"LEFT",11);
+		binaryTree.insert(13,"LEFt",12);
+		binaryTree.insert(16,"Right",10);
+		System.out.println(binaryTree.search(14));
+		System.out.println(binaryTree.search(12));
+		System.out.println(binaryTree.search(16));
+		binaryTree.delete(10);
+		System.out.println(binaryTree.search(10));
+	}
+}
