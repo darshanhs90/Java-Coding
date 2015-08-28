69915f9731379376d0992cdbf756b99130aed44b
darshanhs90 <hsdars@gmail.com>
2015-08-28 12:55:24 +0530

    increasing frame order mainainer pending

---
+++ src/geeksforgeeks/_01AmazonInterview_04_03.java
@@ -0,0 +1,24 @@
+package geeksforgeeks;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+
+
+
+/*
+ * http://www.geeksforgeeks.org/amazon-interview-set-3/
+ * There are n frames of m data element each. The data element in each frame is arranged in increasing order.
+ * You are provided m*n space in which you have to arrange all data in increasing order.
+ */
+public class _01AmazonInterview_04_03 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		int m=Integer.parseInt(scanner.nextLine());
+		int n=Integer.parseInt(scanner.nextLine());
+		scanner.close();
+		
+	}
+
+}
