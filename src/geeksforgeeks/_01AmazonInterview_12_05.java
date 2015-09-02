c98a33314a7b0b915389c1c54c2515afbbe038d1
darshanhs90 <hsdars@gmail.com>
2015-09-02 23:30:16 +0530

    12-05

---
+++ src/geeksforgeeks/_01AmazonInterview_12_05.java
@@ -0,0 +1,16 @@
+package geeksforgeeks;
+
+
+/*
+ * http://www.geeksforgeeks.org/amazon-interview-set-12/
+ * http://www.geeksforgeeks.org/merge-k-sorted-arrays/
+ * There are m sorted arrays of each size n.
+ * You have another array B of size m*n. 
+ * Fill the array B from the m arrays in sorted order.Give the optimal solution.
+ */;
+ public class _01AmazonInterview_12_05 {
+	 public static void main(String[] args) {
+		// use min heap to sort and add all the elements
+	 }
+
+ }
---
+++ src/geeksforgeeks/_01AmazonInterview_13_01.java
@@ -0,0 +1,22 @@
+package geeksforgeeks;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+/*
+ * http://www.geeksforgeeks.org/amazon-interview-set-13/
+ * For a given number, find the next greatest number which is just greater than previous one and made up of same digits.
+ */;
+ public class _01AmazonInterview_13_01 {
+	 public static void main(String[] args) {
+		 Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		 int inputNumber=Integer.parseInt(scanner.nextLine());
+		 scanner.close();
+		 String inputString=String.valueOf(inputNumber);
+		 for (int i = 0; i <inputString.length(); i++) {
+
+		 }
+	 }
+
+ }
