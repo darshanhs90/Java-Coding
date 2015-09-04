a427d2f4a3d1fcb4887b933dd54c481bcbbf0400
darshanhs90 <hsdars@gmail.com>
2015-09-05 02:25:46 +0530

    spiral array generalised

--- src/geeksforgeeks/_01AmazonInterview_21_06.java
+++ src/geeksforgeeks/_01AmazonInterview_21_06.java
@@ -1,6 +1,9 @@
 package geeksforgeeks;
 
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Stack;
 
 
@@ -19,16 +22,31 @@ import java.util.Stack;
  */;
  public class _01AmazonInterview_21_06 {
 	 public static void main(String[] args) {
-		 int inputArray[][]={{1,2,3,4,5},
+		 Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		 int noOfRows=Integer.parseInt(scanner.nextLine());
+		 int noOfColumns=Integer.parseInt(scanner.nextLine());
+		 scanner.close();
+		 int inputArray[][]=buildArray(noOfRows,noOfColumns);
+		/* int inputArray[][]={{1,2,3,4,5},
 				 {6,7,8,9,10},
 				 {11,12,13,14,15},
 				 {16,17,18,19,20},
-				 {21,22,23,24,25}};
+				 {21,22,23,24,25}};*/
 		 printArray(inputArray);
 		 System.out.println(printSpiralArray(inputArray));
 	 }
 
-	 private static String printSpiralArray(int[][] inputArray) {
+	 private static int[][] buildArray(int noOfRows, int noOfColumns) {
+		 int [][]inputArray=new int[noOfRows][noOfColumns];
+		 for (int i = 0; i < noOfRows; i++) {
+			for (int j = 0; j < noOfColumns; j++) {
+				inputArray[i][j]=new Random().nextInt(noOfRows*noOfColumns*2);
+			}
+		}
+		return inputArray;
+	}
+
+	private static String printSpiralArray(int[][] inputArray) {
 		 int noOfRows=inputArray.length;
 		 int noOfColumns=inputArray[0].length;
 		 StringBuffer sb=new StringBuffer();
