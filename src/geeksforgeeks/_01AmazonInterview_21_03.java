1573e1824964d4e09f95814e6122bbe18629efc9
darshanhs90 <hsdars@gmail.com>
2015-09-05 01:30:02 +0530

    n trucks and fuel problem completed

--- src/geeksforgeeks/_01AmazonInterview_21_03.java
+++ src/geeksforgeeks/_01AmazonInterview_21_03.java
@@ -18,19 +18,48 @@ package geeksforgeeks;
  public class _01AmazonInterview_21_03 {
 	 public static void main(String[] args) {
 		 int distance[]={6,3,7};
-		 int petrol[]={4,6,3};
+		 int petrol[]={4,6,4};
 		 startingPointLong(distance,petrol);
 	 }
 
 	private static void startingPointLong(int[] distance, int[] petrol) {	
 		
-		for (int i = 0; i < petrol.length; i++) {
-			int startingPoint=i;
-			for (int j = 0; j < petrol.length; j++) {
-				
+		for (int i = 0; i < petrol.length; i++) {//for forward propogation in circle
+			boolean exited=false;
+			int distanceValue=distance[i];
+			int petrolValue=petrol[i];
+			if(distanceValue>petrolValue){
+				continue;
+			}
+			int j=i;
+			int counter=0;
+			while(true && counter<distance.length){
+				counter++;
+				if(i==j){
+					j=(j+1)%(distance.length);
+					continue;
+				}
+				else{
+					if(distanceValue>petrolValue){
+						exited=true;
+						break;
+					}
+					else{
+						petrolValue=petrolValue-distanceValue;
+						distanceValue=0;
+						distanceValue=distance[j];
+						petrolValue+=petrol[j];
+						j=(j+1)%distance.length;
+					}
+				}
+			}
+			if(!exited)
+			{
+				System.out.println(i);
+				return;
 			}
 		}
-		
+		System.out.println("No Path");
 		
 		
 	}
