f6c684e566ad214c6bfbc7d47265b885e78b48b5
darshanhs90 <hsdars@gmail.com>
2015-09-05 02:21:02 +0530

    array spiral print completed

--- src/geeksforgeeks/_01AmazonInterview_21_06.java
+++ src/geeksforgeeks/_01AmazonInterview_21_06.java
@@ -1,5 +1,8 @@
 package geeksforgeeks;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 
 
 
@@ -16,10 +19,54 @@ package geeksforgeeks;
  */;
  public class _01AmazonInterview_21_06 {
 	 public static void main(String[] args) {
-		
-
+		 int inputArray[][]={{1,2,3,4,5},
+				 {6,7,8,9,10},
+				 {11,12,13,14,15},
+				 {16,17,18,19,20},
+				 {21,22,23,24,25}};
+		 printArray(inputArray);
+		 System.out.println(printSpiralArray(inputArray));
 	 }
 
-	
+	 private static String printSpiralArray(int[][] inputArray) {
+		 int noOfRows=inputArray.length;
+		 int noOfColumns=inputArray[0].length;
+		 StringBuffer sb=new StringBuffer();
+		 int topIndex=0,bottomIndex=noOfRows-1;
+		 int leftIndex=0,rightIndex=noOfColumns;
+
+		 for (int i = 0; i <= noOfColumns/2; i++) {
+			 leftIndex=i;
+			 rightIndex--;
+			 Stack<Integer> stack=new Stack<Integer>();
+			 for (int rowNumber = topIndex; rowNumber <=bottomIndex; rowNumber++) {
+				 if(rowNumber==topIndex || rowNumber==bottomIndex){
+					 for (int colNumber = leftIndex; colNumber <=rightIndex; colNumber++) {
+						 if(rowNumber==topIndex)
+							 sb.append(inputArray[rowNumber][colNumber]+" ");
+						 else
+							 stack.push(inputArray[rowNumber][colNumber]);
+					 }
+				 }
+				 else{
+					 stack.push(inputArray[rowNumber][leftIndex]);
+					 sb.append(inputArray[rowNumber][rightIndex]+" ");	
+				 }
+			 }
+			 while(!stack.isEmpty()){
+				 sb.append(stack.pop()+" ");
+			 }
+			 topIndex++;
+			 bottomIndex--;
+		 }
+		 return (sb.toString());
+	 }
+
+	 private static void printArray(int[][] inputArray) {
+		 for (int i = 0; i < inputArray.length; i++) {
+			 System.out.println(Arrays.toString(inputArray[i]));
+		 }
+
+	 }
 
  }
