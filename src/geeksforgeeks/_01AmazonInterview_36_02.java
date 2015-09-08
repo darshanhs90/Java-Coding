0ff1e946956c4ebd7cbc59a4836afeab8cfa0672
darshanhs90 <hsdars@gmail.com>
2015-09-08 05:57:33 +0530

    3 characters max string completed

---
+++ src/geeksforgeeks/_01AmazonInterview_36_02.java
@@ -0,0 +1,12 @@
+package geeksforgeeks;
+
+
+/*
+ * http://www.geeksforgeeks.org/amazon-interview-set-36/
+ * check the validity of sudoku.
+ */;
+ public class _01AmazonInterview_36_02 {
+	 public static void main(String[] args) {
+		
+	 }
+ }
