0591311bfa8808f6b820f722add780ddd5ec1882
darshanhs90 <hsdars@gmail.com>
2015-09-21 10:50:03 +0530

    stack reverse using recursion completed

--- src/geeksforgeeks/_01DataStructures_Stack_02.java
+++ src/geeksforgeeks/_01DataStructures_Stack_02.java
@@ -13,18 +13,35 @@ import java.util.Stack;
  public class _01DataStructures_Stack_02 {	 
 	 public static void main(String[] args) {
 		 Scanner scanner=new Scanner(new InputStreamReader(System.in));
-		 String inputString=scanner.nextLine();
+		 String inputString="darshan";//scanner.nextLine();
 		 scanner.close();
 		 Stack<Character> stack=new Stack<Character>();
 		 stack=addToStack(inputString,stack);
-		 //reverseStackRecursion(stack,"");
-	 }
-	 private static boolean emptyFlag=false;
-	 private static void reverseStackRecursion(Stack<Character> stack,Character c) {
-		 
+		 System.out.println(stack.toString());
+		 reverseStackRecursion(stack);
+		 System.out.println(stack.toString());
+
 	 }
 
-	 private static Stack<Character> addToStack(String inputString,Stack<Character> stack) {
+	 private static void reverseStackRecursion(Stack<Character> stack) {
+		 if(stack.isEmpty()) 
+			 return;
+	     Character temp = stack.pop();
+	     reverseStackRecursion(stack);
+	     insert_at_bottom(stack,temp);
+	 }
+
+	 private static void insert_at_bottom(Stack<Character> stack, Character data) {
+		 if(stack.isEmpty()){
+	          stack.push(data);
+	          return;
+	     }
+	     Character temp=stack.pop();
+	     insert_at_bottom(stack,data);
+	     stack.push(temp);
+	}
+
+	private static Stack<Character> addToStack(String inputString,Stack<Character> stack) {
 		 for (int i = 0; i < inputString.length(); i++) {
 			 stack.push(inputString.charAt(i));
 		 }
