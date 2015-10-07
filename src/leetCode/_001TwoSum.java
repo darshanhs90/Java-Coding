57c7e9dc4573f89177f7ddde9ae05fe6c2bfaf5e
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-07 11:57:12 -0500

    leetcode Two sum completed

---
+++ src/leetCode/_001TwoSum.java
@@ -0,0 +1,43 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class _001TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		int startIndex=0,endIndex=nums.length-1;
+		int mainArray[]=Arrays.copyOf(nums,nums.length);
+		Arrays.sort(nums);
+		while(true){
+
+			if(nums[startIndex]+nums[endIndex]==target)
+				break;
+			if(nums[startIndex]+nums[endIndex]>target)
+			{
+				endIndex--;
+			}
+			else if(nums[startIndex]+nums[endIndex]<target)
+			{
+				startIndex++;
+			}
+		}
+		boolean startIndexFlag=true,endIndexFlag=true;
+
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[startIndex]==mainArray[i] && startIndexFlag){
+				startIndex=i;
+				startIndexFlag=false;
+			}
+			else if(nums[endIndex]==mainArray[i] && endIndexFlag){
+				endIndex=i;
+				endIndexFlag=false;
+			}
+		}
+		int output[]=new int[]{startIndex+1,endIndex+1};
+		Arrays.sort(output);
+		return output;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(twoSum(new int[]{3,2,4},6)));
+	}
+}
