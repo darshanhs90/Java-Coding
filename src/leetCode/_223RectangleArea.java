3c9975c32b55d6d603d4bc7d949bc85e96c895c7
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-20 04:44:07 -0500

    Rectangle area completed

--- src/leetCode/_223RectangleArea.java
+++ src/leetCode/_223RectangleArea.java
@@ -6,17 +6,21 @@ package leetCode;
 
 public class _223RectangleArea {
 	public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-
-		int rectangle1Area=Math.abs(A-C)*Math.abs(B-D);
-		int rectangle2Area=Math.abs(E-G)*Math.abs(F-H);
-		System.out.println(rectangle1Area);
-		System.out.println(rectangle2Area	);
-
-
-		return 0;
+		if(C<E||G<A )
+	        return (G-E)*(H-F) + (C-A)*(D-B);
+	 
+	    if(D<F || H<B)
+	        return (G-E)*(H-F) + (C-A)*(D-B);
+	 
+	    int right = Math.min(C,G);
+	    int left = Math.max(A,E);
+	    int top = Math.min(H,D);
+	    int bottom = Math.max(F,B);
+	 
+	    return (G-E)*(H-F) + (C-A)*(D-B) - (right-left)*(top-bottom);
 	}
 
 	public static void main(String[] args) {
-		computeArea(-3, 0,3, 4, 0, -1, 9, 2);
+		System.out.println(computeArea(-3, 0,3, 4, 0, -1, 9, 2));
 	}
 }
