3d178bf84263261193bb759d043a300cf1e099c8
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-22 14:33:30 -0500

    Binary Trees Identical check completed

---
+++ src/GeeksforGeeksPractice/_0002TreesAreIdentical.java
@@ -0,0 +1,42 @@
+package GeeksforGeeksPractice;
+
+public class _0002TreesAreIdentical {
+	public static void main(String args[]){
+		TreeNode tn=new TreeNode(1);
+		tn.left=new TreeNode(2);
+		tn.right=new TreeNode(3);
+		tn.left.right=new TreeNode(4);
+		tn.left.left=new TreeNode(5);
+		tn.left.left.left=new TreeNode(6);
+		tn.left.left.left.left=new TreeNode(7);
+		TreeNode tn1=new TreeNode(1);
+		tn1.left=new TreeNode(2);
+		tn1.right=new TreeNode(3);
+		tn1.left.right=new TreeNode(4);
+		tn1.left.left=new TreeNode(5);
+		tn1.left.left.left=new TreeNode(6);
+		tn1.left.left.left.left=new TreeNode(8);
+		System.out.println(isIdentical(tn,tn1));
+	}
+	
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+
+	public static boolean isIdentical(TreeNode tn,TreeNode tn1)
+	{
+		if(tn!=null && tn1!=null)
+		{
+			return tn.val==tn1.val && isIdentical(tn.left,tn1.left) && isIdentical(tn.right,tn1.right);
+		}
+		else if(tn==null && tn1!=null||tn!=null && tn1==null)
+		{
+		return false;
+		}
+		else
+			return true;
+	}
+}
