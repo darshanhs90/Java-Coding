e14a14291909afa5294e81c32c203356c3446876
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-28 23:58:43 -0500

    Sum Tree Completed

---
+++ src/GeeksforGeeksPractice/_0021ConvertTreeToSumTree.java
@@ -0,0 +1,43 @@
+package GeeksforGeeksPractice;
+
+public class _0021ConvertTreeToSumTree {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode next;
+		TreeNode(int x) { val = x; }
+	}
+	public static void main(String args[]){
+		TreeNode tn=new TreeNode(10);
+		tn.left=new TreeNode(-2);
+		tn.right=new TreeNode(6);
+		tn.left.left=new TreeNode(8);
+		tn.left.right=new TreeNode(-4);
+		tn.right.left=new TreeNode(7);
+		tn.right.right=new TreeNode(5);
+		preOrder(tn);
+		System.out.println();
+		getSum(tn);
+		preOrder(tn);
+	}
+	private static int getSum(TreeNode tn) {
+		if(tn!=null)
+		{	
+			int old_val=tn.val;
+			tn.val=getSum(tn.left)+getSum(tn.right);
+			return old_val+tn.val;
+		}	
+		return 0;
+	}
+	private static void preOrder(TreeNode tn) {
+		if(tn!=null)
+		{
+			System.out.print(tn.val+"//");
+			preOrder(tn.left);
+			preOrder(tn.right);
+		}
+
+	}
+
+}
