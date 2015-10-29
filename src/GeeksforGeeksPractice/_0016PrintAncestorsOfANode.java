ad613eec448f9aae867ce1c4448a373b1c9be0e1
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-28 20:48:46 -0500

    Print ancestors of a node completed

--- src/GeeksforGeeksPractice/_0016PrintAncestorsOfANode.java
+++ src/GeeksforGeeksPractice/_0016PrintAncestorsOfANode.java
@@ -1,6 +1,9 @@
 package GeeksforGeeksPractice;
 
+import java.util.Arrays;
+
 public class _0016PrintAncestorsOfANode {
+	static int[] path;
 	public static class TreeNode {
 		int val;
 		TreeNode left;
@@ -8,21 +11,28 @@ public class _0016PrintAncestorsOfANode {
 		TreeNode(int x) { val = x; }
 	}
 	public static void main(String args[]){
+		path=new int[1000];
 		TreeNode tn=new TreeNode(50);
 		tn.left=new TreeNode(8);
 		tn.right=new TreeNode(2);
 		tn.left.left=new TreeNode(3);
-		//tn.left.right=new TreeNode(5);
-		//tn.right.left=new TreeNode(1);
+		tn.left.right=new TreeNode(5);
+		tn.right.left=new TreeNode(1);
 		tn.right.right=new TreeNode(30);
-		//getAncestorsOfANode(tn,8,0);
+		getAncestorsOfANode(tn,path,0,30);
 	}
-	public static boolean getAncestorsOfANode(TreeNode tn,int val) {
+	public static void getAncestorsOfANode(TreeNode tn,int[] path,int pathLen,int val) {
 		if(tn!=null)
 		{
-			return false;
+			path[pathLen]=tn.val;
+			pathLen++;
+			if(tn.val==val)
+			{
+				System.out.println(Arrays.toString(Arrays.copyOf(path, pathLen)));
+			}
+			getAncestorsOfANode(tn.left, path, pathLen, val);
+			getAncestorsOfANode(tn.right, path, pathLen, val);			
 		}
-		return true;
 	}
 
 }
