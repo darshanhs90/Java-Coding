dcb354a1558b9f5b2ddfa67ad1092da6e9e3cfc0
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-10 13:26:28 -0600

    Codility package added

---
+++ src/Codility/_0001TCTapeEquilibrium.java
@@ -0,0 +1,24 @@
+package Codility;
+
+
+/*
+ * Link : https://codility.com/programmers/task/tape_equilibrium
+ */
+
+public class _0001TCTapeEquilibrium {
+
+	public static void main(String[] args) {
+		System.out.println(minimise(new int[]{3,1,2,4,3}));
+	}
+
+	private static int minimise(int[] is) {
+		
+		
+		
+		
+		//use left sum =0,right sum as sum of elements of array,
+		//traverse the list to get the difference
+		return 0;
+	}
+
+}
