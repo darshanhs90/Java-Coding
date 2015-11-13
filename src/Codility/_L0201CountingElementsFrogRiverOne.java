27a15567a387e9c9ad9ce407cab40a4c0582268b
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-13 15:22:45 -0600

    Counting elements frog river one completed

--- src/Codility/_L0201CountingElementsFrogRiverOne.java
+++ src/Codility/_L0201CountingElementsFrogRiverOne.java
@@ -1,24 +1,35 @@
 package Codility;
 
+import java.util.HashMap;
 
 /*
  * Author : Haridarshan H.S.
- * Link : https://codility.com/programmers/task/perm_missing_elem
+ * Link : https://codility.com/programmers/task/frog_river_one
  */
 
 public class _L0201CountingElementsFrogRiverOne {
 
 	public static void main(String[] args) {
-		System.out.println(solution(new int[]{2,3,1,5}));
+		System.out.println(solution(5,new int[]{1,3,1,4,2,3,5,4}));
 	}
 
-	public static int solution(int inputArray[])
-	{
-		int sum=0;
+	public static int solution(int X, int[] inputArray)
+	{	
+		HashMap<Integer,Integer> elementMap=new HashMap<Integer,Integer>();
+		int elementCount=0;
 		for (int i = 0; i < inputArray.length; i++) {
-			sum+=inputArray[i];
+			int element=inputArray[i];
+			if(element>=1 && element<=X)
+			{
+				if(!elementMap.containsKey(element)){
+					elementMap.put(element, element);
+					elementCount++;
+				}
+				
+				if(elementCount==X)
+					return i;
+			}
 		}
-		int size=inputArray.length+1;
-		return ((size*(size+1))/2)-sum;
+		return -1;
 	}
 }
