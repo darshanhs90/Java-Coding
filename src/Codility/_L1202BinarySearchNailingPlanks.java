4134f9a3b16b20dd901c016869c4a5cac56f7e8e
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-16 21:05:51 -0600

    Tie Ropes completed

--- src/Codility/_L1202BinarySearchNailingPlanks.java
+++ src/Codility/_L1202BinarySearchNailingPlanks.java
@@ -10,7 +10,7 @@ public class _L1202BinarySearchNailingPlanks {
 		System.out.println(solution(new int[]{1,4,5,8},new int[]{4,5,9,10},new int[]{4,6,7,10,2}));
 	}
 	public static int solution(int[] A, int[] B, int[] C){
-		int count=0,N=B.length;
+		int count=0;
 		for (int i = 0; i < C.length; i++) {
 			if(C[i]<=B[count] && C[i]>=A[count])
 			{
--- src/Codility/_L1402GreedyTieRopes.java
+++ src/Codility/_L1402GreedyTieRopes.java
@@ -7,10 +7,17 @@ package Codility;
 
 public class _L1402GreedyTieRopes {
 	public static void main(String[] args) {
-		System.out.println(solution(6,new int[]{3,4,5,5,2}));
+		System.out.println(solution(4,new int[]{1,2,3,4,1,1,3}));
 	}
-	public static int solution(int K, int[] A)
-	{
-		return 0;
+	public static int solution(int K, int[] A){
+		int count=0,sum=0;
+		for (int i = 0; i < A.length; i++) {
+			sum+=A[i];
+			if(sum>=K){
+				count++;
+				sum=0;
+			}
+		}
+		return count;
 	}
 }
