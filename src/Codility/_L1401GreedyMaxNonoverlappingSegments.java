b68c58b7c843b4d70c73eba62c7372d1919dd5a0
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-16 20:55:13 -0600

    Max non overlapping segments completed

--- src/Codility/_L1401GreedyMaxNonoverlappingSegments.java
+++ src/Codility/_L1401GreedyMaxNonoverlappingSegments.java
@@ -7,11 +7,18 @@ package Codility;
 
 public class _L1401GreedyMaxNonoverlappingSegments {
 	public static void main(String[] args) {
-		System.out.println(solution(new int[]{3,4,5,5,2},new int[]{3,4,5,5,2}));
+		System.out.println(solution(new int[]{1,3,7,9,9},new int[]{5,6,8,9,10}));
 	}
-	public static int solution(int[] A, int[] B)
-	{
-		return 0;
+	public static int solution(int[] A, int[] B){
+		int count=1;
+		int end=B[0];
+		for (int i = 1; i < B.length; i++) {
+			if(A[i]>end){
+				count++;
+				end=B[i];
+			}
+		}
+		return count;
 	}
 
 }
