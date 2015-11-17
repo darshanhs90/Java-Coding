f2d07754bdebeb86f96e6046a811365e9a191147
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-16 20:10:00 -0600

    Number of triangles completed

--- src/Codility/_L1303CaterpillarMethodCountTriangles.java
+++ src/Codility/_L1303CaterpillarMethodCountTriangles.java
@@ -1,5 +1,7 @@
 package Codility;
 
+import java.util.Arrays;
+
 /*
  * Author : Haridarshan H.S.
  * Link : https://codility.com/programmers/task/count_triangles
@@ -7,15 +9,23 @@ package Codility;
 
 public class _L1303CaterpillarMethodCountTriangles {
 	public static void main(String[] args) {
-		System.out.println(solution(new int[]{3,4,5,5,2}));
+		System.out.println(solution(new int[]{10,2,5,1,8,12}));
 	}
-	public static int solution(int[] A)
-	{
+	public static int solution(int[] A){
 		int count=0;
-		
-		
+		Arrays.sort(A);
+		System.out.println(Arrays.toString(A));
+		for (int i = 0; i <A.length-2; i++) {
+			int k=i+2;
+			for (int j = i+1; j < A.length; ++j)
+			{
+				while (k < A.length && A[i] + A[j] > A[k])
+					++k;
+				count += k - j - 1;
+			}
+		}
+
 		return count;
-		
 	}
 
 }
