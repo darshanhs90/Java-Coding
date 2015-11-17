1b703912e5ca4bf7bf9a2464841dee65f471ec04
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-16 19:42:03 -0600

    count distinct slices completed

--- src/Codility/_L1302CaterpillarMethodDistinctSlices.java
+++ src/Codility/_L1302CaterpillarMethodDistinctSlices.java
@@ -11,11 +11,24 @@ public class _L1302CaterpillarMethodDistinctSlices {
 	}
 	public static int solution(int M, int[] A)
 	{
-		
-		
-		
-		
-		return 0;
+		int N = A.length;
+		int[] counters = new int[M+1];
+		int head = 0;
+		int tail = 0;
+		int result = 0;
+		while (tail < N) {
+			// find the most right end of the array for each tail
+			while (head < N && counters[A[head]] != 2) {//repitition of elements
+				counters[A[head]]++;
+				if (counters[A[head]] == 2)
+					break;
+				head++;
+			}
+			result += head - tail;
+			counters[A[tail]] = 0;
+			tail++;
+		}
+		return result;
 	}
 
 }
