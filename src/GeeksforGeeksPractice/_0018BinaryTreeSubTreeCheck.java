6ef674e689a108fd257c7518a846c5a9448009cb
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-21 21:16:19 -0600

    Check binary sub tree completed

--- src/GeeksforGeeksPractice/_0018BinaryTreeSubTreeCheck.java
+++ src/GeeksforGeeksPractice/_0018BinaryTreeSubTreeCheck.java
@@ -22,7 +22,31 @@ public class _0018BinaryTreeSubTreeCheck {
 
 	}
 	public static boolean checkBinarySubTree(TreeNode tn1,TreeNode tn2){
-		return false;
+		tn1=getNode(tn1,tn2);
+		return isSubTree(tn1,tn2);
+	}
+	private static boolean isSubTree(TreeNode tn1, TreeNode tn2) {
+		if(tn1!=null && tn2!=null)
+		{
+			return tn1.val==tn2.val && isSubTree(tn1.left, tn2.left) && isSubTree(tn1.right, tn2.right);
+		}
+		return (tn1!=null ||tn2!=null)?false:true;
+	}
+	private static TreeNode getNode(TreeNode tn1, TreeNode tn2) {
+		if(tn1!=null){
+			if(tn1.val==tn2.val)
+				return tn1;
+			else
+			{
+				if(getNode(tn1.left, tn2)!=null)
+				{
+					return getNode(tn1.left, tn2);
+				}
+				else
+					return getNode(tn1.right, tn2);
+			}
+		}
+		return null;
 	}
 
 }
