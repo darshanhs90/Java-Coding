b72d686d067d1d60dae855c0eea95135d0d728da
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-22 14:20:37 -0600

    max root to leaf path completed redo

--- src/GeeksforGeeksPractice/_0023MaximumSumLeafToRootPath.java
+++ src/GeeksforGeeksPractice/_0023MaximumSumLeafToRootPath.java
@@ -1,5 +1,7 @@
 package GeeksforGeeksPractice;
 
+import java.util.Arrays;
+
 public class _0023MaximumSumLeafToRootPath {
 	public static class TreeNode {
 		int val;
@@ -16,9 +18,31 @@ public class _0023MaximumSumLeafToRootPath {
 		tn.left.right=new TreeNode(-4);
 		System.out.println(maxSumPath(tn));//17
 	}
+	static int[]path=new int[1000];
+	static int sum=Integer.MIN_VALUE;
 	private static int maxSumPath(TreeNode tn) {
-		// TODO Auto-generated method stub
-		return 0;
+		printPath(tn,path,0);
+		return sum;
 	}
-	
+	private static void printPath(TreeNode tn, int[] path, int pathLen) {
+		if(tn!=null)
+		{
+			path[pathLen]=tn.val;
+			pathLen++;
+			if(tn.left==null && tn.right==null)
+			{	
+				System.out.println(Arrays.toString(Arrays.copyOfRange(path,0,pathLen)));
+				int newSum=0;
+				for (int i = 0; i <pathLen; i++) {
+					newSum+=path[i];
+				}
+				if(newSum>sum)
+					sum=newSum;				
+			}
+			printPath(tn.left, path, pathLen);
+			printPath(tn.right, path, pathLen);	
+		}
+
+	}
+
 }
