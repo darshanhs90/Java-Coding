2e83e167ccfcbbee526b73a432bebb63ea097f99
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-27 22:55:25 -0600

    ugly number dp completed

--- src/GeeksforGeeksPractice/_0094UglyNumbers.java
+++ src/GeeksforGeeksPractice/_0094UglyNumbers.java
@@ -5,7 +5,40 @@ package GeeksforGeeksPractice;
  */
 public class _0094UglyNumbers {
 	public static void main(String[] args) {
-		System.out.println(findUglyNumberNaive(10));
+		System.out.println(findUglyNumberNaive(150));
+		System.out.println(findUglyNumberDP(150));
+		
+	}
+
+	private static int findUglyNumberDP(int N) {
+		int[] ugly=new int[N];
+		ugly[0]=1;
+		int i2=0,i3=0,i5=0;
+		int nextm2=ugly[i2]*2;
+		int nextm3=ugly[i3]*3;
+		int nextm5=ugly[i5]*5;
+		int nextUglyNo=0;
+		for (int i = 1; i < N; i++) {
+			nextUglyNo=Math.min(nextm2, Math.min(nextm3, nextm5));
+			ugly[i]=nextUglyNo;	
+			if(nextUglyNo==nextm2)
+			{
+				i2++;
+				nextm2=ugly[i2]*2;
+			}
+			if(nextUglyNo==nextm3)
+			{
+				i3++;
+				nextm3=ugly[i3]*3;
+			}
+			if(nextUglyNo==nextm5)
+			{
+				i5++;
+				nextm5=ugly[i5]*5;
+			}
+			
+		}
+		return nextUglyNo;
 	}
 
 	private static int findUglyNumberNaive(int i) {
