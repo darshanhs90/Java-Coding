3b1a4ddd98f622589b37fbb5c5580d3e3d7a2607
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-28 17:07:01 -0600

    odd timed number completed array

---
+++ src/GeeksforGeeksPractice/_0103OddTimesNumber.java
@@ -0,0 +1,24 @@
+package GeeksforGeeksPractice;
+
+/*
+ * Link : http://www.geeksforgeeks.org/find-the-number-occurring-odd-number-of-times/
+ */
+public class _0103OddTimesNumber {
+	public static void main(String[] args) {
+		int inputArray[]={1, 2, 3, 2, 3, 1, 3};
+		System.out.println(findOddTimedNumber(inputArray));
+		
+	}
+
+	private static int findOddTimedNumber(int[] inputArray) {
+		int result=inputArray[0];
+		for (int i = 1; i < inputArray.length; i++) {
+			result^=inputArray[i];
+		}
+		return result;
+	}
+
+
+
+
+}
