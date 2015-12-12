b81548704eb4a975ed2042a653bbb9c91e4bad82
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-12-12 16:42:32 -0600

    google interview problems added

---
+++ src/Warmup/_60googleInterview1.java
@@ -0,0 +1,16 @@
+package Warmup;
+
+public class _60googleInterview1 {
+
+	public static void main(String[] args) {
+		
+		//starts with 0xxxxxx- no repetitions
+		//starts with 110xxxxx 10xxxxxx
+		//starts with 1110xxxx 10xxxxxx 10xxxxxx
+		//starts with 1111xxxx 10xxxxxx 10xxxxxx 10xxxxxx
+		
+		
+		
+		
+	}
+}
---
+++ src/Warmup/_61googleInterview2.java
@@ -0,0 +1,13 @@
+package Warmup;
+
+public class _61googleInterview2 {
+
+	public static void main(String[] args) {
+		
+		//linkedlist find the one with shorter size,and other one is with lower sum
+		
+		
+		
+		
+	}
+}
