1ee71664063f3c0aa4719333dc31df15cde369ec
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-12-14 19:26:07 -0600

    google interview check string qn added

---
+++ src/Warmup/_63googleInterview4.java
@@ -0,0 +1,24 @@
+package Warmup;
+
+public class _63googleInterview4 {
+
+	public static void main(String[] args) {
+		//find if 2 sequences match
+		//lord->helloworld-true
+		//lorde->helloworld-false
+		String str1="lord";
+		String str2="helloworld";	
+		checkString(str1,str2);
+
+	}
+
+	private static boolean checkString(String str1, String str2) {
+		
+		for (int i = 0; i < str2.length(); i++) {
+			
+		}
+		
+		
+		return false;
+	}
+}
