78b2ffda95c7ef8a9728a9ddc5cb2c0689f877d1
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-12-14 19:33:01 -0600

    check string completed

--- src/Warmup/_63googleInterview4.java
+++ src/Warmup/_63googleInterview4.java
@@ -6,19 +6,21 @@ public class _63googleInterview4 {
 		//find if 2 sequences match
 		//lord->helloworld-true
 		//lorde->helloworld-false
-		String str1="lord";
+		String str1="lord";//lorde
 		String str2="helloworld";	
-		checkString(str1,str2);
+		System.out.println(checkString(str1,str2));
 
 	}
 
 	private static boolean checkString(String str1, String str2) {
-		
+		int index=0;
 		for (int i = 0; i < str2.length(); i++) {
-			
+			if(str2.charAt(i)==str1.charAt(index)){
+				index++;
+			}
+			if(index==str1.length())
+				return true;
 		}
-		
-		
 		return false;
 	}
 }
