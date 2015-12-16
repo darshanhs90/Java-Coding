bac92d4eba8bc9856c4bf422cad865bbd27c6493
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-12-15 19:24:52 -0600

    check input method added-todo perform and with certain nos to get output

--- src/Warmup/_60googleInterview1.java
+++ src/Warmup/_60googleInterview1.java
@@ -9,7 +9,13 @@ public class _60googleInterview1 {
 		//starts with 1110xxxx 10xxxxxx 10xxxxxx
 		//starts with 1111xxxx 10xxxxxx 10xxxxxx 10xxxxxx
 		
+		Byte[] inputByteArray =new Byte[]{(byte) 0111111};
+		checkInput(inputByteArray);
 		
+		
+	}
+
+	private static void checkInput(Byte[] inputByteArray) {
 		
 		
 	}
