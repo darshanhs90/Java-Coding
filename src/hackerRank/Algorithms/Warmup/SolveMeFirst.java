76637333359fc483be63e8137d3446c39cf579be
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-12-17 21:03:54 -0600

    hackerrank->algos->warmup->solve me first

---
+++ src/hackerRank/Algorithms/Warmup/SolveMeFirst.java
@@ -0,0 +1,26 @@
+package hackerRank.Algorithms.Warmup;
+
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/solve-me-first
+*/
+public class SolveMeFirst {
+
+	  static int solveMeFirst(int a, int b) {
+	      // Hint: Type return a+b; below
+	      return a+b;
+	   }
+
+	   
+	 public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int a;
+	        a = in.nextInt();
+	        int b;
+	        b = in.nextInt();
+	        int sum;
+	        sum = solveMeFirst(a, b);
+	        System.out.println(sum);
+	   }
+}
