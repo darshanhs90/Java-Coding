5d892f9efd68830265e0cfbd2e60e8895151a913
darshanhs90 <hsdars@gmail.com>
2016-01-31 22:38:33 -0600

    hackerrank->algos->search->sherlock and array

---
+++ src/hackerRank/Algorithms/Search/_01SherlockAndArray.java
@@ -0,0 +1,37 @@
+package hackerRank.Algorithms.Search;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/sherlock-and-array
+ */
+public class _01SherlockAndArray {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		int noOfTestCases=Integer.parseInt(scanner.nextLine());
+		for (int i = 0; i < noOfTestCases; i++) {
+			int arrayLength=Integer.parseInt(scanner.nextLine());
+			String inputArray[]=scanner.nextLine().split(" ");
+			int rightSum=0,leftSum=0;
+			for (int j = 0; j < inputArray.length; j++) {
+				rightSum+=Integer.parseInt(inputArray[j]);
+			}
+			boolean found=false;
+			for (int j = 0; j < inputArray.length; j++) {
+				rightSum-=Integer.parseInt(inputArray[j]);
+				if(leftSum==rightSum)
+				{
+					found=true;
+					break;
+				}
+				else
+				{
+					leftSum+=Integer.parseInt(inputArray[j]);
+				}
+			}
+			System.out.println(found==true?"YES":"NO");
+		}
+	}
+}
