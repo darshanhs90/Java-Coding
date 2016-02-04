00bf24dd1b6d88a116f7bced2c0cc7e30652ed99
darshanhs90 <hsdars@gmail.com>
2016-02-03 20:35:32 -0600

    hackerrank->datastructure->arrays->arrays-ds

--- src/hackerRank/DataStructures/Arrays/_01ArraysDS.java
+++ src/hackerRank/DataStructures/Arrays/_01ArraysDS.java
@@ -1,5 +1,24 @@
 package hackerRank.DataStructures.Arrays;
 
-public class _01ArraysDS {
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+/*
+ * Link:https://www.hackerrank.com/challenges/arrays-ds
+ */
+
+
+public class _01ArraysDS {
+	public static void main(String[] args) {
+
+		Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		int N=Integer.parseInt(scanner.nextLine());
+		int inputArray[]=new int[N];
+		for (int i = 0; i < N; i++) {
+			inputArray[i]=scanner.nextInt();
+		}
+		for (int i = inputArray.length-1; i >=0	; i--) {
+			System.out.print(inputArray[i]+" ");
+		}
+	}
 }
