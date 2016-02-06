4481e7d6ce35ccff26ceef7ba3912f7ff1d95855
darshanhs90 <hsdars@gmail.com>
2016-02-06 00:32:06 -0600

    hackerrank->java->introduction->java int to string

---
+++ src/hackerRank/Java/Introduction/_10JavaIntToString.java
@@ -0,0 +1,18 @@
+package hackerRank.Java.Introduction;
+
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/java-int-to-string
+ */
+
+public class _10JavaIntToString {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int n=scanner.nextInt();
+		//========================//
+		String s=String.valueOf(n);
+		//========================//		
+	}
+
+}
