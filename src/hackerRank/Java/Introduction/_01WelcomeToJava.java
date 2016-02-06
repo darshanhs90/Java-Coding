446379f4abca44ba02585257d13d93bb63c625e1
darshanhs90 <hsdars@gmail.com>
2016-02-05 19:03:34 -0600

    hackerrank->java->introduction->welcome to java

---
+++ src/hackerRank/Java/Introduction/_01WelcomeToJava.java
@@ -0,0 +1,15 @@
+package hackerRank.Java.Introduction;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/welcome-to-java
+ */
+
+public class _01WelcomeToJava {
+
+	public static void main(String []argv)
+	   {
+	      //Write your solution here.
+	       System.out.println("Hello World.");
+	       System.out.println("Hello Java.");
+	   }
+}
