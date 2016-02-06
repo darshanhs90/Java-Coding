9e9bb7d6433d6ac9953466cce1b0efb22f837d7b
darshanhs90 <hsdars@gmail.com>
2016-02-05 19:09:20 -0600

    hackerrank->java->introduction->stdin and stdout1

---
+++ src/hackerRank/Java/Introduction/_02JavaStdinAndStdout1.java
@@ -0,0 +1,16 @@
+package hackerRank.Java.Introduction;
+
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+ */
+
+public class _02JavaStdinAndStdout1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();int b=sc.nextInt();int c=sc.nextInt();
+		System.out.println(a);System.out.println(b);System.out.println(c);
+	}
+}
