0ae64b84c06d0711301957a081ccfa9dae237162
darshanhs90 <hsdars@gmail.com>
2016-02-07 23:50:10 -0600

    hackerrank->java->collections->java 2d array

---
+++ src/hackerRank/Java/Collections/_03Java2DArray.java
@@ -0,0 +1,36 @@
+package hackerRank.Java.Collections;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/java-2d-array
+ */
+
+public class _03Java2DArray {
+	public static void main(String[] args) {
+
+		Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		int inputArray[][]=new int[6][6];
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				inputArray[i][j]=scanner.nextInt();
+			}
+		}
+		int max=Integer.MIN_VALUE;
+		for (int i = 0;i<4;i++) {
+			for (int j = 0; j < 4; j++) {
+				int score=checkScore(i,j,inputArray);
+				if(score>max)
+					max=score;
+			}
+		}
+		System.out.println(max);
+	}
+
+	private static int checkScore(int i,int j,int[][] inputArray) {
+		return inputArray[i][j]+inputArray[i][j+1]+inputArray[i][j+2]+
+				inputArray[i+2][j]+inputArray[i+2][j+1]+inputArray[i+2][j+2]
+						+inputArray[i+1][j+1];
+	}
+}
