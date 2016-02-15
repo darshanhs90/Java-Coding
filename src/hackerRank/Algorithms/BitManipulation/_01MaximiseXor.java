31f585e07448434fd3765efeb130a29077108cd7
darshanhs90 <hsdars@gmail.com>
2016-02-14 19:37:14 -0600

    hackerrank->algorithms->bit manipulation->maximise Xor

---
+++ src/hackerRank/Algorithms/BitManipulation/_01MaximiseXor.java
@@ -0,0 +1,24 @@
+package hackerRank.Algorithms.BitManipulation;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/angry-professor
+ */
+public class _01MaximiseXor {
+
+	public static void main(String ards[])
+	{
+		Scanner scanner=new Scanner(new InputStreamReader(System.in));
+		int L=Integer.parseInt(scanner.nextLine());
+		int R=Integer.parseInt(scanner.nextLine());
+		int max=Integer.MIN_VALUE;
+		for (int i = L; i <= R; i++) {
+			for (int j = L; j <= R; j++) {
+				max=Math.max(max,i^j);
+			}
+		}
+		System.out.println(max);
+	}
+}
