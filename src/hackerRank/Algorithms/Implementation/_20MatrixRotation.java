2ece761483d531b21081d83438f35a6ef8eb34d5
darshanhs90 <hsdars@gmail.com>
2016-02-25 21:06:04 -0600

    hackerrank->algos->implementation->matrix rotation

--- src/hackerRank/Algorithms/Implementation/_20MatrixRotation.java
+++ src/hackerRank/Algorithms/Implementation/_20MatrixRotation.java
@@ -20,24 +20,49 @@ public class _20MatrixRotation {
 			for (int j = 0; j < N; j++) {
 				inputArray[i][j]=scanner.nextInt();
 			}
-			System.out.println(Arrays.toString(inputArray[i]));
 		}
-
-		for (int i = 0; i < R; i++) {
-			inputArray=rotateMatrix(inputArray);
-		}
+		inputArray=rotateMatrix(inputArray,R);
 		printMatrix(inputArray);
 	}
 
-	private static int[][] rotateMatrix(int[][] inputArray) {
-		
-		
-		
-		
-		
-		
-		
-		
+	private static int[][] rotateMatrix(int[][] inputArray,int R) {
+		int topRow=0,bottomRow=inputArray.length-1,leftCol=0,rightCol=inputArray[0].length-1;
+		while(topRow<=bottomRow && leftCol<=rightCol)
+		{
+			//move top row from right to left
+			int count=(bottomRow-topRow+1)*(rightCol-leftCol+1)-1;
+				count=R%count;
+			if(!(R>inputArray.length * inputArray[0].length))
+				count=R;
+			for(int j=0;j<count;j++){
+				int t=0;
+				int tempValue=inputArray[topRow][leftCol];
+				for (int i = leftCol; i <rightCol; i++) {
+					inputArray[topRow][i]=inputArray[topRow][i+1];
+				}
+
+				//move left col from bottom to top
+				for (int i = topRow+1; i <=bottomRow; i++) {
+					t=inputArray[i][leftCol];
+					inputArray[i][leftCol]=tempValue;
+					tempValue=t;
+				}
+				//move bottom row from left to right
+				for (int i = leftCol+1; i <=rightCol; i++) {
+					t=inputArray[bottomRow][i];
+					inputArray[bottomRow][i]=tempValue;
+					tempValue=t;
+				}
+
+				//move right col from bottom to top
+				for (int i = bottomRow-1; i >=topRow; i--) {
+					t=inputArray[i][rightCol];
+					inputArray[i][rightCol]=tempValue;
+					tempValue=t;
+				}
+			}
+			topRow++;bottomRow--;leftCol++;rightCol--;
+		}	
 		return inputArray;
 	}
 
