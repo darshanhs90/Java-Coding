806004f25e1d2250bfe1f67c30050c6464942945
Haridarshan HS <haridarshan.h.s@ericsson.com>
2016-03-09 10:42:32 -0600

    hackerrank->java->collections->java generics

---
+++ src/hackerRank/Java/Collections/_08JavaGenerics.java
@@ -0,0 +1,17 @@
+package hackerRank.Java.Collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/challenges/java-arraylist
+ */
+
+public class _08JavaGenerics {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int noOfLists=Integer.parseInt(scanner.nextLine());
+		ArrayList<ArrayList<Integer>> numberList=new ArrayList<>();
+
+	}
+}
