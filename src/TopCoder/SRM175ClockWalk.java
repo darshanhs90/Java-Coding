b6c9f22b8e1996c30cf9b3761f75e0f6a0750de8
darshanhs90 <hsdars@gmail.com>
2016-04-15 18:56:28 -0500

    Topcoder->SRM 175->CLockWalk

---
+++ src/TopCoder/SRM175ClockWalk.java
@@ -0,0 +1,31 @@
+package TopCoder;
+
+/*
+ * SRM 174 Div2
+ * Link:https://community.topcoder.com/stat?c=problem_statement&pm=2241&rd=4680
+ */
+
+
+
+public class SRM175ClockWalk {
+	public static void main(String[] args) {
+		System.out.println(finalPosition("hhthh"));
+		System.out.println(finalPosition("hhtht"));
+		System.out.println(finalPosition("hthth"));
+		System.out.println(finalPosition("hthhhhh"));
+		System.out.println(finalPosition("hthhthtththhtttthttthhhthtttthh"));
+	}
+	public static int finalPosition(String flips){
+		int value=0;
+		for (int i = 0; i < flips.length(); i++) {
+			if(flips.charAt(i)=='h')
+			{
+				value+=i+1;
+			}else{
+				value-=(i+1);
+			}
+		}
+		value=value%12;
+		return value<=0?12+value:value;
+	}
+}
