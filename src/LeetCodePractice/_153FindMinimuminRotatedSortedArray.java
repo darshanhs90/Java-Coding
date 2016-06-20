5f303834b9ac53dfa040387342caabd6dfb774c8
Haridarshan HS <haridarshan.h.s@ericsson.com>
2016-06-20 15:46:53 -0500

    leetcode

--- src/LeetCodePractice/_153FindMinimuminRotatedSortedArray.java
+++ src/LeetCodePractice/_153FindMinimuminRotatedSortedArray.java
@@ -2,17 +2,26 @@ package LeetCodePractice;
 
 public class _153FindMinimuminRotatedSortedArray {
 	public static void main(String[] args) {
-		System.out.println(findMin(new int[]{4,5,6,7,0,1,2}));
+		System.out.println(findMin(new int[]{3,1,2}));
 	}
 	public static int findMin(int[] nums) {
 		return findMinimum(nums,0,nums.length-1);
 	}
-	private static int findMinimum(int[] nums, int low, int high) {
-		if(low<=high)
-		{
-			
+	private static int findMinimum(int[] arr, int left, int right) {
+		if(left==right)
+		{	
+			return arr[left];
 		}
-		return Integer.MAX_VALUE;
+		if (Math.abs(right - left) == 1)
+			return Math.min(arr[left], arr[right]);
+		int mid=(left+right)/2;
+		if(arr[left]<arr[right])
+			return arr[left];
+		else if(arr[mid]>arr[left])
+			return findMinimum(arr, mid, right);
+		else
+			return findMinimum(arr, left, mid);
+
 	}
 }
 
