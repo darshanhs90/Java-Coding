5b82b61e7e6efec9e5fe6eaa5ec9ae65be53d176
darshanhs90 <hsdars@gmail.com>
2016-08-24 01:05:51 -0500

    leetcode problem added

---
+++ src/LeetCodePerformancePractice/_022GenerateParentheses.java
@@ -0,0 +1,26 @@
+package LeetCodePerformancePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _022GenerateParentheses {
+	public static void main(String[] args) {
+		System.out.println(generateParenthesis(3));
+	}
+	static List<String> outputList;
+	public static List<String> generateParenthesis(int n) {
+		outputList=new ArrayList<>();
+		generateParentheses(0,0,n,"");
+		return outputList;
+	}
+	private static void generateParentheses(int forwardBraceCount, int backwardBraceCount,int count, String string) {
+		if(backwardBraceCount>forwardBraceCount||forwardBraceCount>count||backwardBraceCount>count)
+			return;
+		if(forwardBraceCount==count && backwardBraceCount==count){
+			outputList.add(string);
+		}
+		generateParentheses(forwardBraceCount, backwardBraceCount+1,count, string+")");
+		generateParentheses(forwardBraceCount+1, backwardBraceCount,count, string+"(");
+	}
+}
+
