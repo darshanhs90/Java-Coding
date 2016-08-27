92e78d08cad0093e6468a566c8fc5a8c5eb2b4eb
darshanhs90 <hsdars@gmail.com>
2016-08-26 23:43:04 -0500

    leetcode

--- src/LeetCodePerformancePractice/_024SwapNodesInPairs.java
+++ src/LeetCodePerformancePractice/_024SwapNodesInPairs.java
@@ -1,12 +1,49 @@
 package LeetCodePerformancePractice;
 
 public class _024SwapNodesInPairs {
-	public static void main(String[] args) {
-		
-		
-		
-		
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
 	}
+	public static void main(String[] args) {
+		ListNode ln=new ListNode(1);
+		ln.next=new ListNode(2);
+		ln.next.next=new ListNode(3);
+		/*ln.next.next.next=new ListNode(4);*/
+		ln=swapPairs(ln);
+		while(ln!=null)
+		{
+			System.out.println(ln.val);ln=ln.next;
+		}
+	}
+	public static ListNode swapPairs(ListNode head) {
+		if(head==null||head.next==null)
+			return head;
+		ListNode headPointer=head.next;
+		ListNode curr=head,prev=null;
+		while(curr!=null && curr.next!=null)
+		{
+			ListNode currNextNext=curr.next.next;
+			ListNode currNext=curr.next;
+			if(prev==null)
+			{
+				currNext.next=curr;
+				curr.next=currNextNext;
+			}
+			else{
+				currNext.next=curr;
+				curr.next=currNextNext;
+				prev.next=currNext;
+			}
+			prev=curr;
+			curr=curr.next;
+		}
+		return headPointer;
+	}
+	
 
+	
+	
 }
 
---
+++ src/LeetCodePerformancePractice/_027RemoveElement.java
@@ -0,0 +1,29 @@
+package LeetCodePerformancePractice;
+
+import java.util.Arrays;
+
+public class _027RemoveElement {
+	public static void main(String[] args) {
+		System.out.println(removeElement(new int[]{3,2,2,3},3));
+	}
+	public static int removeElement(int[] nums, int val) {
+		if(nums==null||nums.length==0)
+			return 0;
+
+		int index=0,count=0;
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i]!=val)
+			{
+				nums[index]=nums[i];
+				index++;
+				count+=1;
+			}
+		}
+		System.out.println(Arrays.toString(nums));
+		return count;
+	}	
+
+
+
+}
+
