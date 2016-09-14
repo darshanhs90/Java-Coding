11f93a9164104ddc8e0cb098c0ac1da73e84002c
darshanhs90 <hsdars@gmail.com>
2016-09-14 00:39:42 -0500

    leetcode

--- src/LeetCodePerformancePractice/_226InvertBinaryTree.java
+++ src/LeetCodePerformancePractice/_226InvertBinaryTree.java
@@ -1,7 +1,40 @@
 package LeetCodePerformancePractice;
 
 public class _226InvertBinaryTree {
-    public List<String> summaryRanges(int[] nums) {
-        
-    }
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	public static void main(String[] args) {
+		TreeNode tn=new TreeNode(4);
+		tn.left=new TreeNode(2);
+		tn.right=new TreeNode(7);
+		tn.left.left=new TreeNode(1);
+		tn.left.right=new TreeNode(3);
+		tn.right.left=new TreeNode(6);
+		tn.right.right=new TreeNode(9);
+		preOrder(tn);
+		tn=invertTree(tn);
+		System.out.println();
+		preOrder(tn);
+	}
+	private static void preOrder(TreeNode tn) {
+		if(tn!=null)
+		{
+			preOrder(tn.left);
+			System.out.print(tn.val+"/");
+			preOrder(tn.right);
+		}
+	}
+	public static TreeNode invertTree(TreeNode root) {
+		if(root==null)
+			return root;
+		TreeNode left=invertTree(root.left);
+		TreeNode right=invertTree(root.right);
+		root.left=right;
+		root.right=left;
+		return root;	
+	}
 }
