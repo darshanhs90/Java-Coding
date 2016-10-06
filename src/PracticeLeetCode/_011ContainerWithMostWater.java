1f7aa832ceb81ca0b71748344d34961df3858c6d
Haridarshan HS <haridarshan.h.s@ericsson.com>
2016-10-06 12:24:04 -0500

    leetcode

--- src/PracticeLeetCode/_011ContainerWithMostWater.java
+++ src/PracticeLeetCode/_011ContainerWithMostWater.java
@@ -5,7 +5,22 @@ public class _011ContainerWithMostWater {
 	public static void main(String[] args) {
 		System.out.println(maxArea(new int[]{1,1}));
 	}
+	public static int maxArea(int[] height) {
+		if(height==null||height.length==0)
+			return 0;
+		int maxArea=0;
+		int left=0,right=height.length-1;
+		while(left<right){
+		    maxArea=Math.max(maxArea,(right-left)* Math.min(height[right],height[left]));
+			if(height[left]<height[right])
+			{
+				left++;
+			}else{
+				right--;
+			}
+		}
+		return maxArea;
+	}
 
-	
-    
+
 }
--- src/PracticeLeetCode/_014LongestCommonPrefix.java
+++ src/PracticeLeetCode/_014LongestCommonPrefix.java
@@ -3,10 +3,28 @@ package PracticeLeetCode;
 public class _014LongestCommonPrefix {
 
 	public static void main(String[] args) {
-		System.out.println(longestCommonPrefix(new String[]{}));
+		System.out.println(longestCommonPrefix(new String[]{"abb","abc"}));
+	}
+	public static String longestCommonPrefix(String[] strs) {
+		if(strs==null||strs.length==0)
+			return "";
+		int minPrefixLength=strs[0].length();
+		for (int i = 0; i < strs.length; i++) {
+			minPrefixLength=Math.min(minPrefixLength,strs[i].length());
+		}
+
+		for (int i = 0; i < minPrefixLength; i++) {
+			char c=strs[0].charAt(i);
+			for (int j = 1; j < strs.length; j++) {
+				if(strs[j].charAt(i)!=c)
+				{
+					return strs[j].substring(0, i);
+				}
+			}
+		}
+		return strs[0].substring(0, minPrefixLength);
 	}
 
-	
 
 
 }
