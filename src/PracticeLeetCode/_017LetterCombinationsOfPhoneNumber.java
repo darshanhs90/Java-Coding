092d5cf69045cc380ff77b00ed302ca1137ffa9a
Haridarshan HS <haridarshan.h.s@ericsson.com>
2016-10-06 15:34:08 -0500

    leetcode

--- src/PracticeLeetCode/_017LetterCombinationsOfPhoneNumber.java
+++ src/PracticeLeetCode/_017LetterCombinationsOfPhoneNumber.java
@@ -11,6 +11,35 @@ public class _017LetterCombinationsOfPhoneNumber {
 		System.out.println(letterCombinations(""));
 		System.out.println(letterCombinations("23"));
 	}
-	
+	static Map<Character,String> map;
+	public static List<String> letterCombinations(String digits) {
+		List<String> outputList=new ArrayList<>();
+		if(digits==null||digits.length()==0)
+			return outputList;
+		map=new HashMap<>();
+		map.put('1', "1");
+		map.put('2', "abc");
+		map.put('3', "def");
+		map.put('4', "ghi");
+		map.put('5', "jkl");
+		map.put('6', "mno");
+		map.put('7', "pqrs");
+		map.put('8', "tuv");
+		map.put('9', "wxyz");
+		getLetterCombinations(new StringBuilder(),digits,0,outputList);
+		return outputList;
+	}
+	private static void getLetterCombinations(StringBuilder prefix,String digits, int index, List<String> outputList) {
+		if(index>digits.length()-1){
+			outputList.add(prefix.toString());
+			return;
+		}
+		String str=map.get(digits.charAt(index));
+		for (int i = 0; i < str.length(); i++) {
+			prefix.append(str.charAt(i));
+			getLetterCombinations(prefix, digits, index+1, outputList);
+			prefix.deleteCharAt(prefix.length()-1);
+		}
+	}
 
 }
