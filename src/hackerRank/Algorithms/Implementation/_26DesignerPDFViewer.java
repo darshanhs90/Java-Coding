871a1b04973c4c0fd46a73ff342b6f4004200c61
Haridarshan Hosakere Sudhakar <hahosake@microsoft.com>
2016-11-30 12:21:04 -0800

    merger conflict resolved

---
+++ src/hackerRank/Algorithms/Implementation/_26DesignerPDFViewer.java
@@ -0,0 +1,17 @@
+package hackerRank.Algorithms.Implementation;
+
+import java.util.Scanner;
+/*
+ * https://www.hackerrank.com/challenges/designer-pdf-viewer
+ */
+public class _26DesignerPDFViewer {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = 26;
+        int h[] = new int[n];
+        for(int h_i=0; h_i < n; h_i++){
+            h[h_i] = in.nextInt();
+        }
+        String word = in.next();
+    }
+}
