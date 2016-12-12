5e4e62d3c1cdddf01703a6404a54ad717db3a9d2
darshanhs90 <hsdars@gmail.com>
2016-12-11 19:13:22 -0800

    week of code 26

---
+++ src/hackerRank/WeekOfCode26/_06HardHomework.java
@@ -0,0 +1,25 @@
+package hackerRank.WeekOfCode26;
+
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/contests/w26/challenges/hard-homework
+ */
+public class _06HardHomework {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		double max=Double.MIN_VALUE;
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				if(i+j>=n)
+					break;
+				int k=n-i-j;
+				max=Math.max(max, Math.sin(i)+Math.sin(j)+Math.sin(k));
+			}
+		}
+		System.out.printf("%.9f", max);
+	}
+}
+
---
+++ src/hackerRank/WeekOfCode26/_07TastesLikeWinning.java
@@ -0,0 +1,32 @@
+package hackerRank.WeekOfCode26;
+
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/contests/w26/challenges/hard-homework
+ */
+public class _07TastesLikeWinning {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m=in.nextInt();
+		m=(int) Math.pow(2, m);
+		long value=calculateCombination(m);
+		long value2=calculateCombination(m-n);
+		long value3=calculateCombination(n);
+		long output=value/(value2*value3);
+		output=output%(1000000000+7);
+		System.out.println(output);
+		
+	}
+
+	private static long calculateCombination(int n) {
+		long value=1;
+		for (int i = 1; i <=n; i++) {
+			value=value*i;
+		}
+		return value;
+	}
+}
+
