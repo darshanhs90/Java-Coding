7c42129a9e4070a84def26d5f8b827764c9a7271
darshanhs90 <hsdars@gmail.com>
2016-12-19 23:49:27 -0600

    week of code 27 drawing book

---
+++ src/hackerRank/WeekOfCode27/_01DrawingBook.java
@@ -0,0 +1,21 @@
+package hackerRank.WeekOfCode27;
+
+import java.util.Scanner;
+
+/*
+ * Link:https://www.hackerrank.com/contests/w27/challenges/drawing-book
+ */
+public class _01DrawingBook {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int p = in.nextInt();
+		// your code goes here
+		
+		int distanceFromStart=n/2-(p/2);
+		int distanceFromEnd=p/2;
+		System.out.println(Math.min(distanceFromStart, distanceFromEnd));
+	}
+}
+
