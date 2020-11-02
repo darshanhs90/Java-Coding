230d8f1f62039f5b406dad834bf01cbe3451f4ef
darshanhs90 <hsdars@gmail.com>
2020-11-01 19:48:50 -0800

    update

---
+++ src/Leetcode2020Nov/_0141LinkedListCycle.java
@@ -0,0 +1,18 @@
+package Leetcode2020Nov;
+
+public class _0141LinkedListCycle {
+	 public static class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) {
+		          val = x;
+		          next = null;
+		      }
+		  }
+	public static void main(String[] args) {
+	}
+
+    public static boolean hasCycle(ListNode head) {
+        
+    }
+}
